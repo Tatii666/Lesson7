682de36faac0e87fa8481786a1969efae4703fa1
Tatiana <tatianabezgina@icloud.com>
2020-08-10 12:16:10 +0300

    самостоятельно не разобралась со значением boolean, делала по примеру.

--- src/homeWork/Cat.java
+++ src/homeWork/Cat.java
@@ -1,5 +1,40 @@
 package homeWork;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean isFull;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.isFull = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        if(plate.getFood() > appetite) {
+            plate.decreaseFood(appetite);
+            isFull = true;
+        }
+//        System.out.println(name + " eat");
+//        plate.decreaseFood(appetite);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{"  + name +
+                ", appetite=" + appetite +
+                ", isFull=" + isFull +
+                '}';
+    }
+
+    //    public void fear(Dog dog) {
+//        System.out.println(name + " fear " + dog.getName());
+//    }
 
 }
